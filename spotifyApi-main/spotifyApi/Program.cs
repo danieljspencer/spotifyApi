ebc5963264e4a8a6a8936e03919e60c659d909d4
Daniel Spencer <47607407+danieljspencer@users.noreply.github.com>
2023-04-23 08:11:28 +0100

    First basic API call

--- spotifyApi-main/spotifyApi/Program.cs
+++ spotifyApi-main/spotifyApi/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+class Program
+{
+    static async Task Main()
+    {
+      var spotify = new SpotifyClient("BQD1F0qnRBtMhO52xRXGemkCIkCIu94I19Ts3V6vrf19MOkUc_IApORAzDr2oi2Sg5H5rCrO1t3EDVAm5pK_hQW3ppxBn8jKb7kDD31VBOhD3koi6NC7");
+
+      var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+      Console.WriteLine(track.Name);
+    }
+}
